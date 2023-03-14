1ff0b34ea823fd65c8f38a40c9684f1ad103bf24
Nick Stielau <nick.stielau@gmail.com>
2023-03-13 19:47:34 -0700

    Drawbrdige.

---
+++ drawbridge/assets/axios.7fd07793.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.809ca425.js";import{a}from"./axios.f1f32898.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
