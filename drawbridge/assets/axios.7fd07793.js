f2b0c2fd82526b216a73e281d6183526bc709c43
Nick Stielau <nick.stielau@gmail.com>
2023-03-27 22:55:39 -0700

    Redirecting to project page.

--- drawbridge/assets/axios.7fd07793.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./index.809ca425.js";import{a}from"./axios.f1f32898.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
